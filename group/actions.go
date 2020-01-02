a112e87ae7ddee2bc006aa8768c3b440e0c56e30
Simon Richardson <stickupkid@gmail.com>
2020-01-02 17:12:30 +0000

    Adding group exec
    Create a zero-value run.Group, and then add actions to it.
    Actions are defined as a pair of functions: an execute function,
    which should run synchronously; and an interrupt function, which,
    when invoked, should cause the execute function to return.
    
    Finally, invoke Run, which concurrently runs all of the action,
    waits until the first action exits, invokes the interrupt functions,
    and finally returns control to the caller only once all action have
    returned.
    
    This general-purpose API allows callers to model pretty much any
    runnable task, and achieve well-defined lifecycle semantics for the
    group.

---
+++ group/actions.go
@@ -0,0 +1,43 @@
+package group
+
+import (
+	"errors"
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+// Block creates a blocking executional group that waits until some other group
+// action finishing, causing the closing of the group action.
+func Block(g *Group) {
+	cancel := make(chan struct{})
+	g.Add(func() error {
+		<-cancel
+		return nil
+	}, func(error) {
+		close(cancel)
+	})
+}
+
+// Interrupt creates a blocking executional group that becomes free if a
+// interupt or terminate os signal is received.
+func Interrupt(g *Group) {
+	cancel := make(chan struct{})
+	g.Add(func() error {
+		return interrupt(cancel)
+	}, func(error) {
+		close(cancel)
+	})
+}
+
+func interrupt(cancel <-chan struct{}) error {
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, syscall.SIGINT, syscall.SIGTERM)
+	select {
+	case sig := <-c:
+		return fmt.Errorf("received signal %s", sig)
+	case <-cancel:
+		return errors.New("canceled")
+	}
+}
